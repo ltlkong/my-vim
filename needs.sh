9f472101235b5dc4485822d71b7f9ff9ad7e7b03
ltlkong <tielinli@yahoo.com>
2021-08-21 17:38:23 -0500

    format codes

--- needs.sh
+++ needs.sh
@@ -3,10 +3,9 @@ sudo apt update
 RED='\033[0;31m'
 NC='\033[0m'
 
-
 echo -e  ${RED}start installing ripgrep${NC}
 sudo apt install -y ripgrep
-echo -e ${RED}finish installing ripgrep${NC}
+echo -e ${RED}finish${NC}
 
 echo -e ${RED}start installing minimap${NC}
 MINIMAPLINKAMD64='code-minimap-v0.6.1-i686-unknown-linux-musl'
@@ -27,19 +26,18 @@ case $(uname -m) in
 	*) installMiniMap $MINIMAPLINKAMD64
 		;;
 esac
-
-echo -e ${RED}finish installing minimp${NC}
+echo -e ${RED}finish${NC}
 
 echo -e ${RED}start setting up vimrc${NC}
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cat my.vim > ~/.vimrc
 vim +silent +PlugInstall +CocInstall +qa
-echo -e ${RED}finish setting up vimrc${NC}
+echo -e ${RED}finish${NC}
 
 echo -e ${RED}start setting coc-setting${NC}
 sudo cp ./coc-settings.json ~/.vim/
-echo -e ${RED}end setting coc-setting${NC}
+echo -e ${RED}finish${NC}
 
 echo -e ${RED}success${NC}
 
