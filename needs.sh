d5e0230182cfae191c605092948100a4da0f5ce6
ltlkong <tielinli@yahoo.com>
2021-08-21 12:42:31 -0500

    orgainze shell script

--- needs.sh
+++ needs.sh
@@ -1,16 +1,22 @@
 sudo apt update
+
+echo start installing ripgrep
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sudo apt install -y ripgrep
+echo finish installing ripgrep
+
+echo start installing minimap
 wget https://github.com/wfxr/code-minimap/releases/download/v0.6.1/code-minimap-v0.6.1-i686-unknown-linux-musl.tar.gz
 tar -xf code-minimap-v0.6.1-i686-unknown-linux-musl.tar.gz
-cd code-minimap-v0.6.1-i686-unknown-linux-musl
-sudo cp code-minimap /usr/local/bin
-cd ..
+sudo cp code-minimap-v0.6.1-i686-unknown-linux-musl/code-minimap /usr/local/bin
 sudo rm -r code-minimap* 
+echo finish installing minimp
+
 echo start setting up vimrc
 cat my.vim > ~/.vimrc
 vim +PluginInstall +qall
 echo finish setting up vimrc
+
 echo success
 
