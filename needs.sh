e4f034aeb332b7103e098f195e7ba20b9c201232
ltlkong <tielinli@yahoo.com>
2021-08-21 14:55:28 -0500

    install minimap for arm

--- needs.sh
+++ needs.sh
@@ -1,16 +1,25 @@
 sudo apt update
 
+BuildC = $(uname -m)
+
 echo start installing ripgrep
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sudo apt install -y ripgrep
 echo finish installing ripgrep
 
 echo start installing minimap
-wget https://github.com/wfxr/code-minimap/releases/download/v0.6.1/code-minimap-v0.6.1-i686-unknown-linux-musl.tar.gz
-tar -xf code-minimap-v0.6.1-i686-unknown-linux-musl.tar.gz
-sudo cp code-minimap-v0.6.1-i686-unknown-linux-musl/code-minimap /usr/local/bin
-sudo rm -r code-minimap* 
+MINIMAPLINKAMD64='https://github.com/wfxr/code-minimap/releases/download/v0.6.1/code-minimap-v0.6.1-i686-unknown-linux-musl.tar.gz'
+MINIMAPLINKAARCH64='https://github.com/wfxr/code-minimap/releases/download/v0.6.1/code-minimap-v0.6.1-aarch64-unknown-linux-gnu.tar.gz'
+
+if [ $BuildC = 'aarch64' ]
+then 
+	wget $MINIMAPLINKAARCH64 -o minimap.tar.gz
+else 
+	wget $MINIMAPLINKAMD64 -o minimap.tar.gz
+fi
+
+tar -xf minimap.tar.gz 
+sudo cp minimap/code-minimap /usr/local/bin
+sudo rm -r minimap* 
 echo finish installing minimp
 
 echo start setting up vimrc
