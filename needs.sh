57b7e33296affa9ee534e08a4531009e5ee93e8a
root <root@ltlWindows.localdomain>
2021-08-21 01:53:56 -0500

    added vim plug

--- needs.sh
+++ needs.sh
@@ -1,4 +1,6 @@
 sudo apt update
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sudo apt install -y ripgrep
 wget https://github.com/wfxr/code-minimap/releases/download/v0.6.1/code-minimap-v0.6.1-i686-unknown-linux-musl.tar.gz
 tar -xf code-minimap-v0.6.1-i686-unknown-linux-musl.tar.gz
