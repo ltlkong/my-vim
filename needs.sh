021be685bcb1792e6547c6ea5d2938839d633679
ltlkong <tielinli@yahoo.com>
2021-08-21 17:58:28 -0500

    install node

--- needs.sh
+++ needs.sh
@@ -8,24 +8,47 @@ sudo apt install -y ripgrep
 echo -e ${RED}finish${NC}
 
 echo -e ${RED}start installing minimap${NC}
-MINIMAPLINKAMD64='code-minimap-v0.6.1-i686-unknown-linux-musl'
-MINIMAPLINKAARCH64='code-minimap-v0.6.1-aarch64-unknown-linux-gnu'
+MINIMAPAMD64='code-minimap-v0.6.1-i686-unknown-linux-musl'
+MINIMAPAARCH64='code-minimap-v0.6.1-aarch64-unknown-linux-gnu'
 
 installMiniMap() {
 	local minimapBaseUrl='https://github.com/wfxr/code-minimap/releases/download/v0.6.1/'
 
-	wget $minimapBaseUrl$1'.tar.gz'
-	tar -xf $1'.tar.gz'
-	sudo cp $1'/code-minimap' '/usr/local/bin'
-	sudo rm -r code-minimap*
+	wget $minimapBaseUrl$1.tar.gz
+	tar -xf $1.tar.gz
+	sudo cp ./$1/code-minimap /usr/local/bin
+	sudo rm -r $1**
 }
 
 case $(uname -m) in
-	'aarch64') installMiniMap $MINIMAPLINKAARCH64
+	'aarch64') installMiniMap $MINIMAPAARCH64
 		;;
-	*) installMiniMap $MINIMAPLINKAMD64
+	*) installMiniMap $MINIMAPAMD64
 		;;
 esac
+echo -e ${RED}finish${NC}
+
+echo -e ${RED}start installing node${NC}
+sudo apt remove -y nodejs
+NODEAMD64='node-v16.7.0-linux-x64'
+NODEAARCH64='node-v16.7.0-linux-arm64'
+
+installNode() {
+	local nodeBaseUrl='https://nodejs.org/dist/v16.7.0/'
+
+	wget $nodeBaseUrl$1.tar.gz
+	tar -xf ./$1.tar.gz
+	sudo cp -r ./$1/* /usr/local
+	sudo rm -r $1*
+}
+
+case $(uname -m) in
+	'aarch64') installNode $NODEAARCH64
+		;;
+	*) installNode $NODEAMD64
+		;;
+esac
+
 echo -e ${RED}finish${NC}
 
 echo -e ${RED}start setting up vimrc${NC}
